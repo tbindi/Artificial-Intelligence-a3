3e865ba77ee5fb713300c0515ef33d89e62c6fe2
Supreeth S <supreeth90@gmail.com>
2016-11-03 18:17:43 -0400

    Initial table commit

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -12,6 +12,7 @@
 
 import random
 import math
+from pprint import pprint
 
 # We've set up a suggested code structure, but feel free to change it. Just
 # make sure your code still works with the label.py and pos_scorer.py code
@@ -19,15 +20,61 @@ import math
 #
 class Solver:
 
+    WORD_DET = 'det'
+    WORD_VERB = 'verb'
+    WORD_ADP = 'adp'
+    WORD_ADV = 'adv'
+    WORD_ADJ = 'adj'
+    WORD_NOUN = 'noun'
+    WORD_DOT = '.'
+
+
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
     def posterior(self, sentence, label):
         return 0
 
+    def get_blank_dict(self):
+        blank_dict = dict()
+        blank_dict[Solver.WORD_DET] = 0
+        blank_dict[Solver.WORD_VERB] = 0
+        blank_dict[Solver.WORD_ADP] = 0
+        blank_dict[Solver.WORD_ADV] = 0
+        blank_dict[Solver.WORD_ADJ] = 0
+        blank_dict[Solver.WORD_NOUN] = 0
+        blank_dict[Solver.WORD_DOT] = 0
+        return blank_dict
+
+    def __init__(self):
+        self.word_dict = dict()
+
     # Do the training!
     #
     def train(self, data):
-        pass
+
+        for sentence in data:
+            for word_index, word in enumerate(sentence[0]):
+                if word not in self.word_dict:
+                    self.word_dict[word] = self.get_blank_dict()
+
+                if sentence[1][word_index] == Solver.WORD_DET:
+                    self.word_dict[word][Solver.WORD_DET] += 1
+                elif sentence[1][word_index] == Solver.WORD_VERB:
+                    self.word_dict[word][Solver.WORD_VERB] += 1
+                elif sentence[1][word_index] == Solver.WORD_ADP:
+                    self.word_dict[word][Solver.WORD_ADP] += 1
+                elif sentence[1][word_index] == Solver.WORD_ADJ:
+                    self.word_dict[word][Solver.WORD_ADJ] += 1
+                elif sentence[1][word_index] == Solver.WORD_ADV:
+                    self.word_dict[word][Solver.WORD_ADV] += 1
+                elif sentence[1][word_index] == Solver.WORD_NOUN:
+                    self.word_dict[word][Solver.WORD_NOUN] += 1
+                elif sentence[1][word_index] == Solver.WORD_DOT:
+                    self.word_dict[word][Solver.WORD_DOT] += 1
+
+        for word in self.word_dict:
+            pprint(word)
+            pprint(self.word_dict[word])
 
     # Functions for each algorithm.
     #
