6986ab0d20c4e6e4ecb0dca3fed1f4498f5e3d95
Thanmai Bindi <thanmai.m17@gmail.com>
2016-11-05 17:32:53 -0400

    Probability Distribution

--- part2/mountain.py
+++ part2/mountain.py
@@ -9,6 +9,8 @@ from numpy import *
 from scipy.ndimage import filters
 from scipy.misc import imsave
 import sys
+import numpy as np
+
 
 # calculate "Edge strength map" of an image
 #
@@ -18,34 +20,63 @@ def edge_strength(input_image):
     filters.sobel(grayscale,0,filtered_y)
     return filtered_y**2
 
+
 # draw a "line" on an image (actually just plot the given y-coordinates
 #  for each x-coordinate)
 # - image is the image to draw on
-# - y_coordinates is a list, containing the y-coordinates and length equal to the x dimension size
+# - y_coordinates is a list, containing the y-coordinates and length equal to
+# the x dimension size
 #   of the image
-# - color is a (red, green, blue) color triple (e.g. (255, 0, 0) would be pure red
+# - color is a (red, green, blue) color triple (e.g. (255, 0, 0) would be pure
+# red
 # - thickness is thickness of line in pixels
 #
 def draw_edge(image, y_coordinates, color, thickness):
     for (x, y) in enumerate(y_coordinates):
-        for t in range( max(y-thickness/2, 0), min(y+thickness/2, image.size[1]-1 ) ):
+        for t in range(max(y-thickness/2,0),min(y+thickness/2,image.size[1]-1)):
             image.putpixel((x, t), color)
     return image
 
+
+def distribution(data):
+    probability_dist = np.zeros(data.shape)
+    (row, col) = data.shape
+    for j in range(0, col):
+        sum_ = np.sum(data[:, j])
+        for i in range(0, row):
+            probability_dist[i][j] = data[i][j] / sum_
+    return probability_dist
+
+
 # main program
 #
-(input_filename, output_filename, gt_row, gt_col) = sys.argv[1:]
+# (input_filename, output_filename, gt_row, gt_col) = sys.argv[1:]
+def main(inp_data):
+    # load in image
+    for i in inp_data:
+        input_image = Image.open(i[0])
 
-# load in image 
-input_image = Image.open(input_filename)
+        # compute edge strength mask
+        edge_strengt = edge_strength(input_image)
+        prob_dist = distribution(edge_strengt)
+        imsave('edges.jpg', edge_strengt)
 
-# compute edge strength mask
-edge_strength = edge_strength(input_image)
-imsave('edges.jpg', edge_strength)
+        # You'll need to add code here to figure out the results! For now,
+        # just create a horizontal centered line.
+        ridge = edge_strengt.argmax(axis=0).tolist()
 
-# You'll need to add code here to figure out the results! For now,
-# just create a horizontal centered line.
-ridge = [ edge_strength.shape[0]/2 ] * edge_strength.shape[1]
+        # output answer
+        imsave(i[1], draw_edge(input_image, ridge, (255, 0, 0), 5))
 
-# output answer
-imsave(output_filename, draw_edge(input_image, ridge, (255, 0, 0), 5))
+
+if __name__ == "__main__":
+    inp_data = [["test_images/mountain.jpg", "out/mountain_out.jpg"],
+                ["test_images/mountain2.jpg", "out/mountain_out2.jpg"],
+                ["test_images/mountain3.jpg", "out/mountain_out3.jpg"],
+                ["test_images/mountain4.jpg", "out/mountain_out4.jpg"],
+                ["test_images/mountain5.jpg", "out/mountain_out5.jpg"],
+                ["test_images/mountain6.jpg", "out/mountain_out6.jpg"],
+                ["test_images/mountain7.jpg", "out/mountain_out7.jpg"],
+                ["test_images/mountain8.jpg", "out/mountain_out8.jpg"],
+                ["test_images/mountain9.jpg", "out/mountain_out9.jpg"]]
+    main(inp_data)
