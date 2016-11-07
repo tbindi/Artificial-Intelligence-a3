2e407b41c9eb6349cbac05cd4713b0232ee56701
Thanmai Bindi <thanmai.m17@gmail.com>
2016-11-06 23:37:04 -0500

    Final Commit. Enjoy!

--- part2/mountain.py
+++ part2/mountain.py
@@ -3,7 +3,44 @@
 # Mountain ridge finder
 # Based on skeleton code by D. Crandall, Oct 2016
 #
+"""
+(1) a description of how you formulated the problem, including precisely
+defining the abstractions (e.g. HMM formulation);
+    HMM Formulation:
+        - First I choose the initial state as a random column.
+        - Later I will consider the point(row) with the maximum edge weight.
+        - Emission probability is done initially with probability distribution
+        of the edge weight. Edge_weight[Row][Column]/ Sum of EdgeWeight[Column]
+        - I am using Gibbs Sampling to get a sample and move forward while
+        doing n iterations. (our case 50 iterations)
+        - Probability to consider the next point is:
+            Prob = Emission_Probability / (Row Diff)
+        - The Row difference or distance is indirectly proportional to the
+        probability of choosing that row.
 
+(2) a brief description of how your program works;
+    Max Ridge will give us the Maximum value at each column. That is
+    represented by red. Blue represents gibbs sampling which will calculate
+    the probability at the next row compared to the current row. Gibbs sample
+    takes number of iterations, edge weight and rows of top edge weight.
+    Once it will get the column it will move ahead and behind creating the
+    ridge.
+    Green ridge will be using the same formula but instead of picking random
+    point it will start at a point and traverse. This will also make the
+    gibbs sample as just one iteration.
+
+(3) a discussion of any problems, assumptions, simplifications, and/or design
+decisions you made;
+    This problem assumes that the point given by the user will always lie on
+    the ridge so that we traverse only the rows + or - n number of the times.
+    For design we used numpy array for each of the sample, After all sample
+    gets calculated we select the point which is most frequent in each of the
+    column.
+
+(4) answers to any questions asked below in the assignment.
+    --
+
+"""
 from PIL import Image
 from numpy import *
 from scipy.ndimage import filters
@@ -11,6 +48,7 @@ from scipy.misc import imsave
 import numpy as np
 import random
 import sys
+from copy import deepcopy
 
 
 # calculate "Edge strength map" of an image
@@ -53,11 +91,9 @@ def get_random_col():
     return random.randint(0, m-1)
 
 
-def get_probability(prob_dist, row, col, row1, col1, edge_weight, flag):
-    if flag:
-        return prob_dist[row1][col1] / ((1 + abs(row1 - row)) * (1 + abs(
-            (edge_weight[row1][col1])/np.sum(edge_weight[:, col1]) -
-            (edge_weight[row][col])/np.sum(edge_weight[:, col]))))
+def get_probability(prob_dist, row, col, row1, col1):
+    if abs(row1 - row) > 5:
+        return prob_dist[row1][col1] / ((1 + abs(row1 - row)) * 50)
     else:
         return prob_dist[row1][col1] / (1 + abs(row1 - row))
 
@@ -66,40 +102,46 @@ def get_max_row(edge_weight, col):
     return np.argmax(edge_weight[:, col])
 
 
-def get_top_row(edge_weight, col, top):
-    return edge_weight[:, col].argsort()[-top:][::-1]
+def get_top_row(edge_weight, col, top, prev_row, flag):
+    # return edge_weight[:, col].argsort()[-top:][::-1]
+    if flag:
+        return [prev_row-i for i in range(1, top)] + [prev_row] + [prev_row+i
+                                                                 for i in
+                                                                 range(1, top)]
+    else:
+        return edge_weight[:, col].argsort()[-top:][::-1]
 
 
 def create_sample(prob_dist, edge_weight, rand_col, prev_row, top_rows, flag):
     sample = np.zeros((m, ), dtype=np.int)
     sample[rand_col] = prev_row
+    cur_row = deepcopy(prev_row)
     # Move Ahead
     for col in range(rand_col+1, m):
         max_prob = -1
         max_row = -1
         prev_col = col-1
-        for row in get_top_row(edge_weight, col, top_rows):
-            cur_prob = get_probability(prob_dist, prev_row, prev_col, row,
-                                       col, edge_weight, flag)
+        for row in get_top_row(edge_weight, col, top_rows, cur_row, flag):
+            cur_prob = get_probability(prob_dist, cur_row, prev_col, row, col)
             if max_prob < cur_prob:
                 max_prob = cur_prob
                 max_row = row
-        prev_row = max_row
+        cur_row = max_row
         sample[col] = max_row
 
-    prev_row = get_max_row(edge_weight, rand_col)
+    cur_row = deepcopy(prev_row)
+    # prev_row = get_max_row(edge_weight, rand_col)
     # Move behind
     for col in range(rand_col-1, -1, -1):
         max_prob = -1
         max_row = -1
         prev_col = col - 1
-        for row in get_top_row(edge_weight, col, top_rows):
-            cur_prob = get_probability(prob_dist, prev_row, prev_col, row,
-                                       col, edge_weight, flag)
+        for row in get_top_row(edge_weight, col, top_rows, cur_row, flag):
+            cur_prob = get_probability(prob_dist, cur_row, prev_col, row, col)
             if max_prob < cur_prob:
                 max_prob = cur_prob
                 max_row = row
-        prev_row = max_row
+        cur_row = max_row
         sample[col] = max_row
     return sample
 
@@ -125,8 +167,8 @@ gt_col = -1
 (input_filename, output_filename, gt_row, gt_col) = sys.argv[1:]
 m = -1
 n = -1
-iterations = 30
-top_rows = 100
+iterations = 20
+top_rows = 5
 # load in image
 inp_data = [[input_filename, output_filename]]
 for i in inp_data:
